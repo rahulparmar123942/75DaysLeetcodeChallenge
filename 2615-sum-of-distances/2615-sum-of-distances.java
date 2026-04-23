32e2d67c3e1143d6865631fd12dcb7ce7fe0a2c5
rahulparmar123942 <jmatestseries2002@gmail.com>
2026-04-23 20:59:07 +0530

    Time: 23 ms (64.57%), Space: 122.4 MB (43.5%) - LeetHub

---
+++ 2615-sum-of-distances/2615-sum-of-distances.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public long[] distance(int[] arr) {
+        int n = arr.length;
+        long[] res = new long[n];
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.computeIfAbsent(arr[i], k -> new ArrayList<>()).add(i);
+        }
+        for (List<Integer> list : map.values()) {
+
+            int size = list.size();
+            long[] prefix = new long[size];
+            prefix[0] = list.get(0);
+            for (int i = 1; i < size; i++) {
+                prefix[i] = prefix[i - 1] + list.get(i);
+            }
+            for (int i = 0; i < size; i++) {
+                int index = list.get(i);
+                long left = (long) index * i - (i > 0 ? prefix[i - 1] : 0);
+                long right = (prefix[size - 1] - prefix[i]) - (long) index * (size - i - 1);
+                res[index] = left + right;
+            }
+        }
+        return res;
+    }
+}
