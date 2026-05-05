4bc70be880e735ef002896cc74dc4f6cdc68a384
rahulparmar123942 <jmatestseries2002@gmail.com>
2026-05-05 19:00:29 +0530

    Time: 0 ms (100%), Space: 43.9 MB (16.14%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,6 +1,6 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
+       int n = matrix.length;
      
        for(int i =0;i<n-1;i++){
         for(int j =i+1;j<n;j++){
@@ -23,7 +23,7 @@ class Solution {
             row[end] =temp;
             st++;
             end--;
-
            }
+           
         }
 }
