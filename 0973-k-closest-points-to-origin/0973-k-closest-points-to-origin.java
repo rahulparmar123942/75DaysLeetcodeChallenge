cc1559cfa664066a6ae06cdd0e76c9692e196702
rahulparmar123942 <jmatestseries2002@gmail.com>
2026-07-27 22:02:17 +0530

    Time: 32 ms (36.12%), Space: 59.5 MB (11.8%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,35 @@
+import java.util.PriorityQueue;
+class Triplet implements Comparable <Triplet>{
+    int dist;
+    int x;
+    int y;
+    Triplet(int dist,int x,int y){
+        this.x = x;
+        this.y = y;
+        this.dist = dist;
+    }
+    public int compareTo(Triplet t){
+        return t.dist - this.dist;
+    }
+}
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        
+        PriorityQueue<Triplet> pq = new PriorityQueue<>();
+
+        for(int[] point : points){
+            int x = point[0],y = point[1];
+
+            int dist = x*x+y*y;
+            pq.add(new Triplet(dist,x,y));
+            if(pq.size()>k)pq.remove();
+        }
+        int[][] ans = new int[k][2];
+        for(int i =0;i<k;i++){
+            Triplet top = pq.remove();
+            ans[i][0] = top.x;
+            ans[i][1] = top.y;
+        }
+         return ans;
+    }
+}
