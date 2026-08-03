d3cd95f8f29b48b9770b723ade872349ff38dc49
rahulparmar123942 <jmatestseries2002@gmail.com>
2026-08-03 21:13:05 +0530

    Time: 0 ms (100%), Space: 42.5 MB (95.17%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,26 @@
+class Solution {
+    static int[] dp;
+
+    public int rob(int[] arr) {
+        int n = arr.length;
+        dp = new int[n];
+        Arrays.fill(dp, -1);
+        return loot(0, arr);
+    }
+
+    public int loot(int i, int[] arr) {
+        if (i >= arr.length)
+            return 0;
+
+        if (dp[i] != -1)
+            return dp[i];
+
+        int pick = arr[i] + loot(i + 2, arr);
+        int skip = loot(i + 1, arr);
+
+        int ans = Math.max(pick, skip);
+
+        dp[i] = ans;
+        return ans;
+    }
+}
