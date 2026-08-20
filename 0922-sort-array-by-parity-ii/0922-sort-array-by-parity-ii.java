1b14378f37cec576e59157775360fba5918eb5a9
rahulparmar123942 <jmatestseries2002@gmail.com>
2026-08-20 23:52:42 +0530

    Time: 2 ms (99.82%), Space: 48.6 MB (57.66%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
@@ -2,24 +2,20 @@ class Solution {
     public int[] sortArrayByParityII(int[] nums) {
         
         int n = nums.length;
-        Arrays.sort(nums);
-        ArrayList<Integer> ans1 = new ArrayList<>();
-        ArrayList<Integer> ans2 = new ArrayList<>();
+      int[] arr = new int[n];
 
-        for(int i=0;i<n;i++){
-            if(nums[i]%2==0){
-                ans1.add(nums[i]);
-            }else{
-                ans2.add(nums[i]);
-            }
+      int even =0;
+      int odd =1;
+      for(int i=0;i<n;i++){
+
+        if(nums[i]%2==0){
+           arr[even] = nums[i];
+           even +=2;
+        }else{
+            arr[odd] = nums[i];
+            odd +=2;
         }
-        
-        int even =0;
-        int odd =0;
-        for(int j =0;j<n;j++){
-           if(j%2==0) nums[j] = ans1.get(even++);
-           else nums[j] = ans2.get(odd++);
-        }
-        return nums;
+      }
+      return arr;
     }
 }
