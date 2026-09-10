3a60b30acb2769dfd0e6bd546b5dbd137fb59691
rahulparmar123942 <jmatestseries2002@gmail.com>
2026-09-10 16:53:10 +0530

    Time: 15 ms (74.23%), Space: 48.1 MB (31.93%) - LeetHub

---
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean canBeValid(String s, String locked) {
+        
+       int n = s.length();
+ 
+       if(n%2!=0) return false;
+       
+       int balanced =0;
+       for(int i=0;i<n;i++){
+             if(locked.charAt(i)=='0' || s.charAt(i)=='('){
+                balanced++;
+             }else{
+                balanced--;
+             }
+             if(balanced<0) return false;
+       }
+       
+       balanced =0;
+       for(int i=n-1;i>=0;i--){
+        if(locked.charAt(i)=='0' || s.charAt(i) ==')'){
+            balanced++;
+        }else{
+            balanced--;
+        }
+        if(balanced<0) return false;
+       }
+    
+       return true;
+    }
+}
