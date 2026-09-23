23a5911708de54ab2cc97d5b4dbdfb3b05a45199
rahulparmar123942 <jmatestseries2002@gmail.com>
2026-09-24 00:54:31 +0530

    Time: 2 ms (92.6%), Space: 65.2 MB (70.24%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,20 +1,34 @@
 class Solution {
-    public int[] productExceptSelf(int[] num) {
-        int n = num.length;
-        int[] ans = new int[n];
+    public int[] productExceptSelf(int[] nums) {
+        
+        int n = nums.length;
+
         int[] prefix = new int[n];
         int[] suffix = new int[n];
+        int[] ans = new int[n];
+
          prefix[0] = 1;
         for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]*num[i-1];
+            prefix[i] = prefix[i-1] * nums[i-1];
         }
-         suffix[n - 1] = 1;
+
+        suffix[n-1] = 1;
         for(int i=n-2;i>=0;i--){
-            suffix[i]=suffix[i+1]*num[i+1];
-           }
+            suffix[i] = suffix[i+1]*nums[i+1];
+        }
+
         for(int i=0;i<n;i++){
-            ans[i]=prefix[i]*suffix[i];
+            ans[i] = prefix[i]*suffix[i];
         }
-        return ans; 
-        }
+        return ans;
     }
+}
+// prefix[0] = 1 
+// prefix[1] = 1*2 = 2
+// prefix[2] = 2*3 = 6
+// prefix[3] = 6*4 = 24
+
+// suffix[3] = 4*3=12
+// suffix[2] = 12*2 = 24
+// suffix[1] = 24*2 = 48
+//suffix[0] = 48*1 = 48
